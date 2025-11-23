d3a065cd3b305c9de3bdf19dca9a56bc7ec12cff
Eduardo Amaral <eduamaralmp@gmail.com>
2025-11-23 16:06:57 -0300

    OAB

--- components/sobre-nos.tsx
+++ components/sobre-nos.tsx
@@ -15,6 +15,9 @@ export function SobreNos() {
             <p className="text-lg text-foreground">
               Profissional especializado em Direito do Trabalho, preparado para defender seus direitos.
             </p>
+              <p className="text-lg text-foreground">
+              OAB/TO: 14000100
+            </p>
           </div>
 
           <div className="flex flex-col items-center text-center">
